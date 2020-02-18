31278ec65ff41d19f17350041d3fb7b92a2c0d32
chukwude <ebukamiracle35@gmail.com>
2020-02-18 10:19:57 +0000

    make it better

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const path = require('path');
 const loanRequest = require('./LoanRequest/Loan_Request');
 const userInfoPost = require('./LoanRequest/SaveRequestForLoan');
 
-
 //for messageing Auth
 const app = express();
 
@@ -27,33 +26,32 @@ app.use('/requestloan', loanRequest);
 app.use('/useInfo_forLoan', userInfoPost);
 
 //error handling
-app.use((req,res)=>{
+app.use((req, res) => {
 	res.json({
 		message: 'page not found'
-	})
+	});
 });
 
 const port = process.env.PORT || 800;
 //for number verification
 
-
-
 //    "mongodb://Miracle:4hrZlimYiuxGAMk1@cluster0-shard-00-00-rz247.mongodb.net:27017,cluster0-shard-00-01-rz247.mongodb.net:27017,cluster0-shard-00-02-rz247.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
 // '//mongodb://localhost:27017/smartLoan'
-mongoose.connect(process.env.MONGODB_URL || '//mongodb://localhost:27017/smartLoan' , {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useUnifiedTopology: true
-}, () => {
-	console.log('connect to mogoDb');
-});
+mongoose.connect(
+	'mongodb://Miracle:4hrZlimYiuxGAMk1@cluster0-shard-00-00-rz247.mongodb.net:27017,cluster0-shard-00-01-rz247.mongodb.net:27017,cluster0-shard-00-02-rz247.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+	{
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true
+	},
+	() => {
+		console.log('connect to mogoDb');
+	}
+);
 
-
-if (process.env.NODE_ENV === "Production"){
-
-		console.log('poduction')
+if (process.env.NODE_ENV === 'Production') {
+	console.log('poduction');
 }
-
 
 app.listen(port, () => {
 	console.log('listing to port 800');
